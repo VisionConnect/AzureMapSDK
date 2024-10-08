30612600a2702afa84a49c740f081bd897aaa33c
VisionConnect <erik@visionlandscapeservices.com>
2024-10-08 19:55:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
+# Enable detailed logging
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/')
 def index():
+    app.logger.info("Serving index.html")
     return render_template('index.html')  # This will serve your index.html
 
 if __name__ == '__main__':
     app.run(debug=False)
+
 
 
