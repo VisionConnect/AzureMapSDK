f1178989e588bdad98ee61bc9325abe936781a4a
Erik Delgado <erik@visionlandscapeservices.com>
2024-10-08 22:21:30 -0400

    Moved index.html to templates and updated app.py for property pins and layer toggles

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import pyodbc
 import logging
 
 app = Flask(__name__)
@@ -6,10 +7,28 @@ app = Flask(__name__)
 # Enable detailed logging
 logging.basicConfig(level=logging.DEBUG)
 
+# Function to get properties from the database
+def get_properties():
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=your_server;DATABASE=Irrigation_db;UID=user;PWD=password')
+    cursor = conn.cursor()
+    cursor.execute("SELECT Property_Name, Latitude, Longitude FROM Properties")
+    properties = cursor.fetchall()
+    conn.close()
+    return [{'name': row[0], 'latitude': row[1], 'longitude': row[2]} for row in properties]
+
+# Flask route to serve the property data as JSON
+@app.route('/properties')
+def properties():
+    app.logger.info("Serving property data")
+    return jsonify(get_properties())
+
+# Route to serve the index.html
 @app.route('/')
 def index():
     app.logger.info("Serving index.html")
-    return render_template('index.html')  # This will serve your index.html
+    return render_template('index.html')
 
+# Start the application
 if __name__ == '__main__':
     app.run(debug=False)
+
