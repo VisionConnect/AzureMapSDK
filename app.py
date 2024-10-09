858c08f2fca384736d3d61a7ecd517c0294d2adf
Erik Delgado <erik@visionlandscapeservices.com>
2024-10-08 21:09:19 -0400

    Updated app.py to bind to default port

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 8000))  # Azure App Service will provide the PORT environment variable
+    app.run(host='0.0.0.0', port=port)
 
